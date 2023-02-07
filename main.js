9a64883cf01aeeccf3911ee6c0933ab3eee1b005
Barbora Vesela <vesela.bara@gmail.com>
2023-02-07 21:35:50 +0100

    hotovy ukol

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 // tady je místo pro náš program
 
+let jmeno = prompt("Your name and surname:");
+let vek = prompt("Your age:");
 
+document.body.innerHTML = "Your name is " + jmeno + " and you are " + vek + " years old.";
 
+let barva = prompt("What is your favourite colour?");
 
+let body = document.querySelector('body')
+body.style.color = barva;
